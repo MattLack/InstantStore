ca0761354d66609c9179b0e1865b79344f7a4435
MattLack <a.mateuslol@gmail.com>
2018-11-17 12:11:49 -0300

    create and link sign up activity

--- feature_app/src/main/java/com/instantstore/instant_store/feature_app/LoginActivity.kt
+++ feature_app/src/main/java/com/instantstore/instant_store/feature_app/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.instantstore.instant_store.feature_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun goToSignUpActvity(view : View){
+        val intent = Intent(applicationContext,SignUpActivity::class.java)
+        // intent.putExtra("input",editText.text.toString())
+        startActivity(intent)
     }
 }
