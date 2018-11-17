b863317a7f8d1f315d1ed87899ae61d9f980f5b6
MattLack <a.mateuslol@gmail.com>
2018-11-17 07:51:49 -0300

    testando login activity

--- feature_app/src/main/java/com/instantstore/instant_store/feature_app/MainActivity.kt
+++ feature_app/src/main/java/com/instantstore/instant_store/feature_app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.instantstore.instant_store.feature_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(applicationContext,LoginActivity::class.java)
+
+        startActivity(intent)
     }
 }
