27d918caa24b26b92e2906a53fdf4e0a7d6615d7
MattLack <a.mateuslol@gmail.com>
2018-11-18 13:56:45 -0300

    switch toolbar to drawer toolbar - no fragmet

--- feature_app/src/main/java/com/instantstore/instant_store/feature_app/MainScreenActivity.kt
+++ feature_app/src/main/java/com/instantstore/instant_store/feature_app/MainScreenActivity.kt
@@ -1,12 +1,18 @@
 package com.instantstore.instant_store.feature_app
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main_screen.*
+import kotlinx.android.synthetic.main.app_bar_drawer.*
 
-class MainScreenActivity : AppCompatActivity() {
+class MainScreenActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,43 +21,36 @@ class MainScreenActivity : AppCompatActivity() {
         // Set the toolbar as support action bar
         setSupportActionBar(toolbar)
 
-        // Now get the support action bar
-        val actionBar = supportActionBar
-
-        // Set toolbar title/app title
-        actionBar!!.title = "Instant App"
-
-        // Set action bar/toolbar sub title
-        //actionBar.subtitle = "App subtitle"
-
-        // Set action bar elevation
-        actionBar.elevation = 4.0F
-
-        // Display the app icon in action bar/toolbar
-        actionBar.setDisplayShowHomeEnabled(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu to use in the action bar
-        val inflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        return super.onCreateOptionsMenu(menu)
+        init()
     }
 
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    private fun init(){
+        val toogle = ActionBarDrawerToggle(Activity(),drawer_layout,toolbar,R.string.nav_open,R.string.nav_close)
+        drawer_layout.addDrawerListener(toogle)
+        toogle.syncState()
+        navigation_view.setNavigationItemSelectedListener(this)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle presses on the action bar menu items
         when (item.itemId) {
             R.id.action_settings -> {
-                text_view.text = "set"
-                return true
+                message.text = "set"
             }
             R.id.action_about_us -> {
-                text_view.text = "about"
-                return true
+                message.text = "about"
+            }
+            R.id.action_messages -> {
+                message.text = "message"
             }
         }
-        return super.onOptionsItemSelected(item)
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+
     }
+
+
 
 }
