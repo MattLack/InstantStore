aee1f8c393ca267eba1c226f58c1dd7460218883
Caio <caiocbv@gmail.com>
2018-11-18 23:21:13 -0300

    Tela de Cadastro com insternacionalizacao e landyscape variation

--- feature_app/src/main/java/com/instantstore/instant_store/feature_app/SignUpActivity.kt
+++ feature_app/src/main/java/com/instantstore/instant_store/feature_app/SignUpActivity.kt
@@ -1,12 +1,20 @@
 package com.instantstore.instant_store.feature_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+    }
+
+    fun goToMainScreenActvity(view : View){
+        val intent = Intent(applicationContext,MainScreenActivity::class.java)
+        // intent.putExtra("input",editText.text.toString())
+        startActivity(intent)
     }
 }
