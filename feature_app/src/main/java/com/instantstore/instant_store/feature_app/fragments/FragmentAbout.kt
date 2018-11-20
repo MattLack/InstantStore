b5e2f6b42fbc5ad6df180bd89f7783d9726526d1
MattLack <a.mateuslol@gmail.com>
2018-11-20 00:41:41 -0300

    create fragment layout about

--- feature_app/src/main/java/com/instantstore/instant_store/feature_app/fragments/FragmentAbout.kt
+++ feature_app/src/main/java/com/instantstore/instant_store/feature_app/fragments/FragmentAbout.kt
@@ -11,11 +11,11 @@ import com.instantstore.instant_store.feature_app.R
 class FragmentAbout : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_1,container, false)
+        val view = inflater.inflate(R.layout.fragment_about,container, false)
 
         val title = view.findViewById<TextView>(R.id.title)
 
-        title.text = "Fragment About"
+        //title.text = "Fragment About"
         //title.setTextColor(resources.getColor(colorAccent, null))
 
         return view
